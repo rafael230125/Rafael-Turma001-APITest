8de232cecbdba4a331b4ff092069f0b34f6dae01
Rafael Carvalho <122944870+rafael230125@users.noreply.github.com>
2025-04-29 20:17:35 -0300

    Update cart.spec.ts

--- test/cart.spec.ts
+++ test/cart.spec.ts
@@ -27,7 +27,7 @@ describe('Cart - POST', () => {
         .spec()
         .post(`${baseUrl}/carts`)
         .withJson({
-          product_id: 'HHJC7RERZ0M3VDGS6X9HM11A', 
+          product_id: 'HHJC7RERZ0M3VDGS6X9HM00A', 
           quantity: 1
         })
         .expectStatus(StatusCodes.CREATED)
